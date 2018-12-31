265b996470d6fcec626300fb704cdc392c7ecb93
Britton <britton@126.com>
2018-12-31 20:41:38 +0800

    BUG Fixed

--- src/main/java/com/tio/app/biz/controllers/MMKKController.java
+++ src/main/java/com/tio/app/biz/controllers/MMKKController.java
@@ -1,5 +1,6 @@
 package com.tio.app.biz.controllers;
 
+import com.tio.app.contents.services.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/pcweb/mm-kk-pp")
 public class MMKKController {
+    @Autowired
+    private TestService testService;
 
     @PostMapping("/tt")
     public String tt() {
-        return "aaaa-bbb-ccc-ddd" ;
+        return "aaaa-bbb-ccc-ddd" + testService.test();
     }
 }
---
+++ src/main/java/com/tio/app/biz/services/BizService.java
@@ -0,0 +1,1 @@
+package com.tio.app.biz.services;
